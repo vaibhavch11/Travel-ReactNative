84eb3c11a189c81ecdaf12c5fc642f71ba630252
unknown <kunal.siwal@gmail.com>
2022-09-22 23:57:34 +0530

    mia didi khush

--- components/SlideKaDabba.js
+++ components/SlideKaDabba.js
@@ -4,10 +4,10 @@ import React from 'react'
 
 const SlideKaDabba = () => {
   return (
-   <TouchableOpacity className='mr-2 mb-2'>
+   <TouchableOpacity className='h-40 mr-2 mb-2 flex-none'>
     <Image 
     source={require('../images/MiaDidi.png')}
-    className=' h-20 w-20 rounded'
+    className=' h-48 w-72 rounded'
     />
    </TouchableOpacity> 
       
